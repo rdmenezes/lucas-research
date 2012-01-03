050809428fc292d0d8fb77313d819dcacb877829
Owen McAree <o.mcaree@lucasresearch.co.uk>
2012-01-03 16:59:04 +0000

    Added some comments, testing linux build

--- XPlaneMAVLink/XPlaneMAVLink/MAVLink.cpp
+++ XPlaneMAVLink/XPlaneMAVLink/MAVLink.cpp
@@ -1,36 +1,64 @@
 #include "MAVLink.h"
 
+/* Constructor for the MAVLink class
+ *	Takes the id's of this system and component and a pointer to the datalink to be used
+ */
 MAVLink::MAVLink(uint8_t mySystemId, uint8_t myComponentId, DataLink *link) {
 	this->mySystemId = mySystemId;
 	this->myComponentId = myComponentId;
 	this->link = link;
 }
 
+/* Set the id of the component to which messages should be sent
+ *	Note this is not implemented on a lot of messages, see the MAVLink documentation
+ *	to figure out which ones
+ */
 void MAVLink::setTargetComponent(uint8_t targetSystemId, uint8_t targetComponentId) {
 	this->targetSystemId = targetSystemId;
 	this->targetComponentId = targetComponentId;
 }
 
+/* Receive a message from the DataLink
+ *	This stores the latest message received for use by the getters
+ *	This function should be called at least once per loop to ensure all packets
+ *	get captured. If running on a PC you should consider threading a call to this
+ *	function so it is called regularly in the background
+ */
 bool MAVLink::receiveMessage() {
 	int msg_len = link->receive(MAVLINK_BUFFER_SIZE,(char*)buffer);
 	for (int i=0; i<msg_len; i++) {
 		if (mavlink_parse_char(0,buffer[i],&msg,&status)) {
-			MAVLinkMessage mm;
-			mm.msg = msg;
-			mm.timeReceived = getTime_ms();
-			MessageMap[msg.msgid] = mm;
-
+			addMessage();
 			return true;
 		}
 	}
 	return false;
 }
 
+/* Add the latest packet to local storage, overriding previous examples
+ *	Time of receipt is captured to allow the timeout conditions to be
+ *	evaluated by the getters
+ */
+void MAVLink::addMessage() {
+	MAVLinkMessage mm;
+	mm.msg = msg;
+	mm.timeReceived = getTime_ms();
+	MessageMap[msg.msgid] = mm;
+}
+
+/* Send a message
+ *	Once the buffer has been filled by a sender this function is called
+ *	to send it to the DataLink.
+ *	Returns true if successful, false otherwise
+ */
 bool MAVLink::sendMessage() {
 	int msg_len = mavlink_msg_to_send_buffer(buffer, &msg);
 	return link->send((char *)buffer, msg_len);
 }
 
+/* Get the time (Milliseconds since midnight)
+ *	TODO: update this to the UNIX epoch
+ */
 int MAVLink::getTime_ms() {
 	SYSTEMTIME st;
 	GetSystemTime(&st);
@@ -40,6 +68,10 @@ int MAVLink::getTime_ms() {
 		+ st.wHour*1000*60*60;
 }
 
+/* Check the for the timeout condition for a specific message
+ *	If a packet was received greater than 'timeout' milliseconds ago
+ *	this function returns false
+ */
 bool MAVLink::checkTimeout(MAVLinkMessage *mm, int timeout) {
 	if ((getTime_ms()-mm->timeReceived)<timeout) {
 		return true;
@@ -47,22 +79,23 @@ bool MAVLink::checkTimeout(MAVLinkMessage *mm, int timeout) {
 	return false;
 }
 
+/*************************** Senders and Getters ***************************/
+/* All Getters are passed parameters by reference and return true is the timeout
+ * condition has not been encoutned (false if timed out)
+ */
 
-
-
-
-
-
+/* Heatbeat */
 bool MAVLink::sendHeartbeat(uint8_t type, uint8_t autopilotType) {
 	mavlink_msg_heartbeat_pack(mySystemId,myComponentId,&msg,type,autopilotType);
 	return sendMessage();
 }
 
+/* Attitude Sender */
 bool MAVLink::sendAttitude(float roll, float pitch, float yaw, float p, float q, float r) {
 	mavlink_msg_attitude_pack(mySystemId,myComponentId,&msg,getTime_ms(),roll,pitch,yaw,p,q,r);
 	return sendMessage();
 }
-
+/* Attitude Getter */
 bool MAVLink::getAttitude(float &roll, float &pitch, float &yaw, float &p, float &q, float &r) {
 	MAVLinkMessage * mm = &MessageMap[MAVLINK_MSG_ID_ATTITUDE];
 	if (mm == NULL) {
@@ -77,10 +110,13 @@ bool MAVLink::getAttitude(float &roll, float &pitch, float &yaw, float &p, float
 	return checkTimeout(mm,MAVLINK_ATTITUDE_TIMEOUT);
 }
 
+/* RC Override Sender */
 bool MAVLink::sendRCOverride(uint16_t c1, uint16_t c2, uint16_t c3, uint16_t c4, uint16_t c5, uint16_t c6, uint16_t c7, uint16_t c8) {
 	mavlink_msg_rc_channels_override_pack(mySystemId,myComponentId,&msg,targetSystemId,targetComponentId,c1,c2,c3,c4,c5,c6,c7,c8);
 	return sendMessage();
 }
+
+/* RC Override Getter */
 bool MAVLink::getRCOverride(uint16_t &c1, uint16_t &c2, uint16_t &c3, uint16_t &c4, uint16_t &c5, uint16_t &c6, uint16_t &c7, uint16_t &c8) {
 	MAVLinkMessage * mm = &MessageMap[MAVLINK_MSG_ID_RC_CHANNELS_OVERRIDE];
 	if (mm == NULL) {
@@ -95,4 +131,48 @@ bool MAVLink::getRCOverride(uint16_t &c1, uint16_t &c2, uint16_t &c3, uint16_t &
 	c7 = mavlink_msg_rc_channels_override_get_chan7_raw(&mm->msg);
 	c8 = mavlink_msg_rc_channels_override_get_chan8_raw(&mm->msg);
 	return checkTimeout(mm,MAVLINK_RC_OVERRIDE_TIMEOUT);
+}
+
+/* Raw GPS Sender */
+bool MAVLink::sendRawGPS(int fix, float lat, float lng, float alt, float eph, float epv, float v, float crs) {
+	mavlink_msg_gps_raw_pack(mySystemId,myComponentId,&msg,getTime_ms(),fix,lat,lng,alt,eph,epv,v,crs);
+	return sendMessage();
+}
+
+/* Raw GPS Getter */
+bool MAVLink::getRawGPS(int &fix, float &lat, float &lng, float &alt, float &eph, float &epv, float &v, float &crs) {
+	MAVLinkMessage * mm = &MessageMap[MAVLINK_MSG_ID_GPS_RAW];
+	if (mm == NULL) {
+		return false;
+	}
+	fix = mavlink_msg_gps_raw_get_fix_type(&mm->msg);
+	lat = mavlink_msg_gps_raw_get_lat(&mm->msg);
+	lng = mavlink_msg_gps_raw_get_lon(&mm->msg);
+	alt = mavlink_msg_gps_raw_get_alt(&mm->msg);
+	eph = mavlink_msg_gps_raw_get_eph(&mm->msg);
+	epv = mavlink_msg_gps_raw_get_epv(&mm->msg);
+	v = mavlink_msg_gps_raw_get_v(&mm->msg);
+	crs = mavlink_msg_gps_raw_get_hdg(&mm->msg);
+	return checkTimeout(mm,MAVLINK_RAW_GPS_TIMEOUT);
+}
+
+/* VFR HUD Sender */
+bool MAVLink::sendVFRHUD(float airspeed, float groundspeed, int16_t heading, uint16_t throttle, float alt, float climb) {
+	mavlink_msg_vfr_hud_pack(mySystemId,myComponentId,&msg,airspeed,groundspeed,heading,throttle,alt,climb);
+	return sendMessage();
+}
+
+/* VFR HUD Getter */
+bool MAVLink::getVFRHUD(float &airspeed, float &groundspeed, int16_t &heading, uint16_t &throttle, float &alt, float &climb) {
+	MAVLinkMessage * mm = &MessageMap[MAVLINK_MSG_ID_VFR_HUD];
+	if (mm == NULL) {
+		return false;
+	}
+	airspeed = mavlink_msg_vfr_hud_get_airspeed(&mm->msg);
+	groundspeed = mavlink_msg_vfr_hud_get_groundspeed(&mm->msg);
+	heading = mavlink_msg_vfr_hud_get_heading(&mm->msg);
+	throttle = mavlink_msg_vfr_hud_get_throttle(&mm->msg);
+	alt = mavlink_msg_vfr_hud_get_alt(&mm->msg);
+	climb = mavlink_msg_vfr_hud_get_climb(&mm->msg);
+	return checkTimeout(mm,MAVLINK_VFR_HUD_TIMEOUT);
 }
--- XPlaneMAVLink/XPlaneMAVLink/MAVLink.h
+++ XPlaneMAVLink/XPlaneMAVLink/MAVLink.h
@@ -1,19 +1,33 @@
+/* MAVLink class to abstract message encoding and decoding
+ *	Message encoding and transmission is handled by simple Sender functions
+ *
+ *	receiveMessage() should be called once per loop (or threaded) to receive the
+ *	latest message from the DataLink. This message is then cached locally and
+ *	decoded when required. Messages are cached with a time of reception to allow
+ *	a timeout condition to be monitor (i.e. was this data received within the last xxx ms)
+ */
+
 #ifndef MAVLink_h
 #define MAVLink_h
-
-#define MAVLINK_BUFFER_SIZE 1024
-#define MAVLINK_ATTITUDE_TIMEOUT 1000
-#define MAVLINK_HEARTBEAT_TIMEOUT 1000
-#define MAVLINK_RC_OVERRIDE_TIMEOUT 1000
-
 
 #include <stdio.h>
 #include <windows.h>
 #include <map>
-#include "mavlink_types.h"//"mavlink_types.h"
+#include "mavlink_types.h"
 #include "common\common.h"
 #include "DataLink.h"
 
+//send/receive buffer size
+#define MAVLINK_BUFFER_SIZE 1024
+
+//timeouts
+#define MAVLINK_ATTITUDE_TIMEOUT 1000
+#define MAVLINK_HEARTBEAT_TIMEOUT 1000
+#define MAVLINK_RC_OVERRIDE_TIMEOUT 1000
+#define MAVLINK_RAW_GPS_TIMEOUT 1000
+#define MAVLINK_VFR_HUD_TIMEOUT 1000
+
+//Structure for local storage of messages
 struct MAVLinkMessage {
 	mavlink_message_t msg;
 	int timeReceived;
@@ -23,27 +37,37 @@ class MAVLink {
 public:
 	__declspec( dllexport ) MAVLink(uint8_t mySystemId, uint8_t myComponentId, DataLink *link);
 	__declspec( dllexport ) void setTargetComponent(uint8_t targetSystemId, uint8_t targetComponentId);
+	__declspec( dllexport ) bool receiveMessage();
+	
+	/* Senders and Getters */
 	__declspec( dllexport ) bool sendHeartbeat(uint8_t type, uint8_t autopilotType);
 	__declspec( dllexport ) bool sendAttitude(float roll, float pitch, float yaw, float p, float q, float r);
 	__declspec( dllexport ) bool getAttitude(float &roll, float &pitch, float &yaw, float &p, float &q, float &r);
 	__declspec( dllexport ) bool sendRCOverride(uint16_t c1, uint16_t c2, uint16_t c3, uint16_t c4, uint16_t c5, uint16_t c6, uint16_t c7, uint16_t c8);
 	__declspec( dllexport ) bool getRCOverride(uint16_t &c1, uint16_t &c2, uint16_t &c3, uint16_t &c4, uint16_t &c5, uint16_t &c6, uint16_t &c7, uint16_t &c8);
-	__declspec( dllexport ) bool receiveMessage();
+	__declspec( dllexport ) bool sendRawGPS(int fix, float lat, float lng, float alt, float eph, float epv, float v, float crs);
+	__declspec( dllexport ) bool getRawGPS(int &fix, float &lat, float &lng, float &alt, float &eph, float &epv, float &v, float &crs);
+	__declspec( dllexport ) bool sendVFRHUD(float airspeed, float groundspeed, int16_t heading, uint16_t throttle, float alt, float climb);
+	__declspec( dllexport ) bool getVFRHUD(float &airspeed, float &groundspeed, int16_t &heading, uint16_t &throttle, float &alt, float &climb);
+	
 private:
+	void addMessage();
 	bool sendMessage();
 	MAVLinkMessage * findMessage(int id);
 	int getTime_ms();
 	bool checkTimeout(MAVLinkMessage *mm, int timeout);
+
 	DataLink *link;
 	uint8_t mySystemId, myComponentId;
 	uint8_t targetSystemId, targetComponentId;
-
 	mavlink_message_t msg;
 	mavlink_status_t status;
 	uint8_t buffer[MAVLINK_BUFFER_SIZE];
-
-//	std::map<int, MAVLinkMessage> MessageMap;
+#ifdef _WIN32 || __linux__
+	std::map<int, MAVLinkMessage> MessageMap;
+#else
 	MAVLinkMessage MessageMap[255];
+#endif
 };
 
 #endif
